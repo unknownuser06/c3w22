d702bfd87a88b520103329e2297820652b172d84
unknowuser06 <shavarblackwood@gmail.com>
2022-01-31 14:56:21 -0500

    Initial

---
+++ script.js
@@ -0,0 +1,19 @@
+//WHEN LOADING IS FINISHED, TRIGGER START
+
+$(window).on("load", start);
+
+function start(){
+    $("ul.nav a:first").click();
+}
+
+$(document).on("click", "ul.nav a", nav);
+
+
+function nav(e){
+    e.preventDefault();
+    //alert("Hello World");
+    var url = $(this).attr("href");
+    $("#content").load(url);
+    $("ul.nav a").removeClass("active");
+    $(this).addClass("active");
+}
